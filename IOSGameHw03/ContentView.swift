87223eac166e38ce39c6b7e483620fa0a745d7a0
lowkey9904 <00657140@email.ntou.edu.tw>
2021-03-25 14:07:43 +0800

    Initial Commit

---
+++ IOSGameHw03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOSGameHw03
+//
+//  Created by Joker on 2021/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOSGameHw03/IOSGameHw03App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSGameHw03App.swift
+//  IOSGameHw03
+//
+//  Created by Joker on 2021/3/25.
+//
+
+import SwiftUI
+
+@main
+struct IOSGameHw03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
